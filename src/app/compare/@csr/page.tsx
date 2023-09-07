3329193fd0c39f8358c2a65cb3fb5daab9a8ddf2
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2023-09-07 18:25:19 +0900

    parallel route layout fix

--- src/app/compare/@csr/page.tsx
+++ src/app/compare/@csr/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import S from "./styles.module.scss";
 import ClientRender from "@/components/Compare/ClientRender";
 
 const page = () => {
-  return <ClientRender path="compare" />;
+  return (
+    <div className={S.csr}>
+      <header>CSR 페이지</header>
+      <ClientRender path="compare" />
+    </div>
+  );
 };
 
 export default page;
