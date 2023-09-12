37b707abf76200aaaa817848310a8283798425be
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2023-09-12 19:39:54 +0900

    route handler deleted

--- src/app/ssr/layout.tsx
+++ src/app/ssr/layout.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import S from "./styles.module.scss";
 
-const getUserList = async () => {
-  const userList = await fetch("http://localhost:3000/api/mongo");
-  return userList;
-};
+// const getUserList = async () => {
+//   const userList = await fetch("http://localhost:3000/api/mongo");
+//   return userList;
+// };
 
 const layoutSSR = async ({ children }: { children: React.ReactNode }) => {
-  const res = await getUserList();
-  const result = await res.json();
+  // const res = await getUserList();
+  // const result = await res.json();
 
   return (
     <div className={S.ssr}>
-      <header>SSR 페이지 {result.name} </header>
+      <header>SSR 페이지</header>
       {children}
     </div>
   );
