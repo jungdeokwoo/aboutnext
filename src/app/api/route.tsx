5f334b0b4f7f34b0b379dde7eea7d3d6428e686a
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2023-09-14 19:17:13 +0900

    test api DELETE

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/MongoDb/dbConnect";
+import { ObjectId } from "mongodb";
 
 export async function GET(req: NextRequest) {
   const client = await connectDB;
@@ -7,4 +8,14 @@ export async function GET(req: NextRequest) {
   const result = await db.collection("nextApi").find().toArray();
 
   return NextResponse.json(result);
+}
+
+export async function DELETE(request: NextRequest) {
+  const body = await request.json();
+  console.log(body, "?");
+  const client = await connectDB;
+  const db = client.db("test");
+  await db.collection("nextApi").deleteOne({ _id: new ObjectId(body._id) });
+
+  return NextResponse.json("delete");
 }
