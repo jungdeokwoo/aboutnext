1966203df2f617edbc20b19f1a8c50aeb2df403d
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2023-09-15 00:25:54 +0900

    change env

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -5,8 +5,8 @@ import { revalidatePath, revalidateTag } from "next/cache";
 import UserCard from "@/components/Compare/UserCard";
 
 const getBoardList = async () => {
-  console.log(process.env.BASE_URL, "<<URL");
-  const response = await fetch(`${process.env.BASE_URL}/api`, {
+  console.log(process.env.DB_, "<<URL");
+  const response = await fetch(`${process.env.DB_BASE_URL}/api`, {
     cache: "no-cache",
     next: { tags: ["boardList"] },
   });
@@ -19,8 +19,8 @@ export default async function Board() {
 
   const deleteHandler = async (formData: FormData) => {
     "use server";
-    console.log(process.env.BASE_URL, "<<URL");
-    const response = await fetch(`${process.env.BASE_URL}/api`, {
+    console.log(process.env.DB_BASE_URL, "<<URL");
+    const response = await fetch(`${process.env.DB_BASE_URL}/api`, {
       method: "DELETE",
       body: JSON.stringify({ _id: formData.get("listId") }),
       cache: "no-cache",
@@ -33,7 +33,7 @@ export default async function Board() {
   const createHandler = async (formData: FormData) => {
     "use server";
     console.log(formData, "<????");
-    const response = await fetch(`${process.env.BASE_URL}/api`, {
+    const response = await fetch(`${process.env.DB_BASE_URL}/api`, {
       method: "POST",
       body: JSON.stringify({
         name: formData.get("name"),
