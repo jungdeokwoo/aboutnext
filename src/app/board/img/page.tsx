8a9f636663b6f8c0d38d8d5b9cb1985c7ce372b7
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2023-09-20 08:42:26 +0900

    img fn modify

--- src/app/board/img/page.tsx
+++ src/app/board/img/page.tsx
@@ -1,38 +1,36 @@
-// "use client";
+"use client";
 
-// import React, { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import S from "./styles.module.scss";
 
 const ImgPage = () => {
-  // const [imgFile, setImgFile] = useState<File[] | null>(null);
+  const [imgFile, setImgFile] = useState<File[] | null>(null);
 
   const postImg = async () => {
     fetch(`${process.env.BASE_URL}/api/img`);
   };
 
-  const formHandler = async () => {
-    "use server";
-    console.log("form");
-  };
-
-  // const imgHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   console.log(event.target.files, ":::::");
-  //   const imgArr = event.target.files;
-  //   if (!imgArr) return;
-  //   const fileList: File[] = Array.from(imgArr);
-  //   setImgFile(fileList);
+  // const formHandler = async () => {
+  //   "use server";
+  //   console.log("form");
   // };
+
+  const imgHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const imgArr = event.target.files;
+    if (!imgArr) return;
+    const fileList: File[] = Array.from(imgArr);
+    setImgFile(fileList);
+  };
 
   return (
     <div className={S.imgWrapper}>
-      {/* {imgFile && <img src={URL.createObjectURL(imgFile[0])} />} */}
+      {imgFile && <img src={URL.createObjectURL(imgFile[0])} />}
       <form>
         <label htmlFor="imgUp">
           <p>이미지 업로드</p>
-          <input id="imgUp" type="file" hidden />
+          <input id="imgUp" type="file" hidden onChange={imgHandler} />
         </label>
-        <button></button>
+        <button>등록하기</button>
       </form>
     </div>
   );
