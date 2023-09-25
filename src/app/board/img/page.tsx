e3d923e89a83fd51bdcf83b99d60444750e9394b
dogsocks <dogsocks@dogsocksui-MacBookPro.local>
2023-09-25 19:29:21 +0900

    delete api setting

--- src/app/board/img/page.tsx
+++ src/app/board/img/page.tsx
@@ -4,22 +4,22 @@ import ImgList from "@/components/Board/ImgList";
 import ImgUpLoad from "@/components/Board/ImgUpLoad";
 import { ImgListProps } from "@/types/Board/type";
 
-const getImgList = async () => {
-  const response = await fetch(`${process.env.BASE_URL}/api/board/img/url`, {
-    cache: "no-cache",
-    next: { tags: ["imgList"] },
-  });
-  const imgList = await response.json();
-  return imgList;
-};
+// const getImgList = async () => {
+//   const response = await fetch(`${process.env.BASE_URL}/api/board/img/url`, {
+//     cache: "no-cache",
+//     next: { tags: ["imgList"] },
+//   });
+//   const imgList = await response.json();
+//   return imgList;
+// };
 
 const ImgPage = async () => {
-  const imgList: ImgListProps[] = await getImgList();
+  // const imgList: ImgListProps[] = await getImgList();
 
   return (
     <div className={S.imgWrapper}>
       <ImgUpLoad />
-      <ImgList imgList={imgList} />
+      {/* <ImgList imgList={imgList} /> */}
     </div>
   );
 };
